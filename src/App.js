ff42e9150bd6ee45b92cb58bcc112c926cbd19fc
Corey Lanier <iam@coreylanier.com>
2019-09-23 17:09:56 -0400

    final 'product'

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import axios from "axios";
 import Search from "./components/Search";
 import "./App.css";
 
@@ -18,9 +19,72 @@ class App extends React.Component {
     });
   };
 
-  searchMatches = e => {
+  searchMatches = async e => {
     e.preventDefault();
-    console.log("searching");
+    const { name } = this.state;
+    let id = null;
+    let matches = null;
+    this.setState({
+      loading: true
+    });
+    await axios({
+      method: "GET",
+      url: "/api/summoners",
+      params: {
+        name
+      }
+    })
+      .then(res => {
+        id = res.data.accountId;
+      })
+      .catch(err => {
+        console.log(err);
+      });
+    await axios({
+      method: "GET",
+      url: "/api/matches",
+      params: {
+        id,
+        perPage: 5
+      }
+    })
+      .then(res => {
+        matches = res.data.matches;
+      })
+      .catch(err => {
+        console.log(err);
+      });
+    await matches.forEach(async match => {
+      await axios({
+        method: "GET",
+        url: "/api/match",
+        params: {
+          id: match.gameId
+        }
+      })
+        .then(res => {
+          const { participantIdentities } = res.data;
+          let participantId = null;
+          participantIdentities.forEach(participant => {
+            if (participant.player.summonerName === name) {
+              participantId = participant.participantId;
+            }
+          });
+          const matchData = res.data.participants[participantId - 1];
+          console.log(matchData);
+          matchData.forEach((match, index) => {
+            matches[index].win = match.stats.win;
+            matches[index].kills = match.stats.kills;
+            matches[index].deaths = match.stats.deaths;
+            matches[index].assists = match.stats.assists;
+            matches[index].largestKillingSpree =
+              match.stats.largestKillingSpree;
+          });
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    });
   };
 
   render() {
